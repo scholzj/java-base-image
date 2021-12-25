614a6579a16b1e679bff9383e2f43e1a498d4bc0
Jakub Scholz <www@scholzj.com>
2021-12-25 18:48:12 +0100

    Fix Vert.x cache config
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- scripts/launch_java.sh
+++ scripts/launch_java.sh
@@ -6,7 +6,7 @@ JAR=$1
 shift
 
 # Make sure that we use /dev/urandom
-JAVA_OPTS="${JAVA_OPTS} -Dvertx.cacheDirBase=/tmp -Djava.security.egd=file:/dev/./urandom"
+JAVA_OPTS="${JAVA_OPTS} -Dvertx.cacheDirBase=/tmp/vertx-cache -Djava.security.egd=file:/dev/./urandom"
 
 # Enable GC logging for memory tracking
 #JAVA_OPTS="${JAVA_OPTS} -XX:NativeMemoryTracking=summary -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps"
